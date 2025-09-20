8037d57e31f840f39ca7856f50ed6a78b669dc05
username <wecp-dummy@gmail.com>
2025-09-20 07:11:40 +0000

    Day 23 Task

--- frontend/src/app/bank/bank.module.ts
+++ frontend/src/app/bank/bank.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from "@angular/common";
 import { BankRoutingModule } from "./bank-routing.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { AccountComponent } from "./components/account/account.component";
+import { CustomersComponent } from "./components/customer/customer.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AccountComponent,
+    CustomersComponent
+  ],
+  schemas: [NO_ERRORS_SCHEMA],
   imports: [
     CommonModule,
     BankRoutingModule,
@@ -14,7 +21,6 @@ import { HttpClientModule } from "@angular/common/http";
     HttpClientModule,
   ],
   exports: [
-    
   ]
 })
 export class BankModule {}
